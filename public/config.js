e6ecbc7f713858b938a74a1c42b920d0a2342d89
18827515336@163.com <18827515336@163.com>
2021-05-12 15:28:45 +0800

    周质控

--- public/config.js
+++ public/config.js
@@ -7,28 +7,29 @@
  * @FilePath: \data_trace\public\config.js
  */
 
+const pageUrl = 'http://183.166.140.243:8087/HS_water_WebUI/QualityControl';
+
 // API接口地址（正式）
 window.API = 'http://183.166.140.243:8088/api';
-
 // API接口地址（测试）
 window.testUrl = 'http://192.168.90.41:8024/api';
 window.configUrl = 'http://192.168.90.55:9082/api';
 
 window.pageList = [{
     title: '零点数据',
-    url: 'http://192.168.90.35:8080/ZeroPointData.html'
+    url: `${pageUrl}/ZeroPointData.html`
   },
   {
     title: '零点数据图表',
-    url: 'http://192.168.90.35:8080/ZeroPointDataChart.html'
+    url: `${pageUrl}/ZeroPointDataChart.html`
   },
   {
     title: '跨度数据',
-    url: 'http://192.168.90.35:8080/SpanData.html'
+    url: `${pageUrl}/SpanData.html`
   },
   {
     title: '跨度数据图表',
-    url: 'http://192.168.90.35:8080/SpanDataChart.html'
+    url: `${pageUrl}/SpanDataChart.html`
   }
 ]
 // 数据详情
