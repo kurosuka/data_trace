426b699240a962846a5918eae4a6fcb1bd48366a
lijiaqi <1162798935@qq.com>
2021-06-03 10:43:08 +0800

    备案表缺陷修改

--- src/components/MultiSelect.vue
+++ src/components/MultiSelect.vue
@@ -41,7 +41,6 @@
         :check-on-click-node="true"
         :filter-node-method="filterNode"
         :expand-on-click-node="false"
-        :default-checked-keys="this.defaultData"
         :default-checked-keys="defaultData"
         @node-click="nodeClick" :props="defaultPropsInner"></el-tree>
       </el-option>
